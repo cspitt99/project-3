505b4c44bdbc1140d0756ffdd42ebd6390913f00
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-15 10:52:20 -0400

    updated language to match the server side

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,9 +13,9 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">The Animal Nest</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Let's get cozy and talk about our pets!</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
