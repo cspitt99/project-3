2c6c25720ff506989f73a33498c3588358d962e5
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-15 12:01:54 -0400

    ran npm functions to update package.json file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+require("dotenv").config()
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb+srv://root:Password1234@cluster0.7oi1h5z.mongodb.net/project3_db?retryWrites=true&w=majority'
     {
