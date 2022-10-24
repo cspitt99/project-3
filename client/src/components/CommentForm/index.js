6977cd68f75a3eacbfde3b9bd6565eb88f5510a8
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-24 00:23:50 -0400

    finalizing changes

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 
 import { ADD_COMMENT } from '../../utils/mutations';
 
